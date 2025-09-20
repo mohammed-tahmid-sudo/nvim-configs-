1de89eadba81bb24d0b55e62b418625aa7b855fd
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-09-20 15:13:32 +0600

    updates

--- lua/plugins_and_config/lsp-config.lua
+++ lua/plugins_and_config/lsp-config.lua
@@ -1,3 +1,39 @@
-return{
-	{"neovim/nvim-lspconfig"}
+-- plugins.lua
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",      -- LSP completion
+      "hrsh7th/cmp-buffer",        -- buffer words
+      "hrsh7th/cmp-path",          -- filesystem paths
+      "hrsh7th/cmp-cmdline",       -- cmdline completion
+      "L3MON4D3/LuaSnip",          -- snippets
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 }
+
